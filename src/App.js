ae8625c1a68d5b1f244625ac6bc3a9a7fee99c6d
Kartik Kusmude <kusmudekartik5418@gmail.com>
2024-10-16 18:15:32 +0530

    defined another way to passdown the STATE variable and setter function of that STATE variable,  of App.js to Todoinput.jsx and TodoList.jx and commented the previous approach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// method 1
+import React, { useState } from 'react';
+import "./App.css";
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
-function App() {
+const App = ()=>{
+  const [taskList,setTaskList] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'main-container'>
+      <div className = 'center-container'>
+        <TodoInput taskList={taskList} setTaskList={setTaskList}  />
+        {
+          taskList.map((ele,index) =>{
+            return <TodoList key={index} current_index={index} list_item={ele}  taskList={taskList} setTaskList={setTaskList} />
+          })
+        }
+      
+      </div>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
+
+
+
+// method 2
+// import React, { useState } from 'react';
+// import "./App.css";
+// import TodoInput from './components/TodoInput';
+// import TodoList from './components/TodoList';
+
+// const App = ()=>{
+//   const [taskList,setTaskList] = useState([]);
+
+//   function add_item_to_list(inputText){
+//     if(inputText.length > 0){
+//       console.log("add_item_to_list function called...");
+//       setTaskList([...taskList, inputText ])
+//     }
+//   }
+
+//   function delete_item_from_list(index){
+
+//     let temp_list = [...taskList];
+//     console.log('temp_list :>> ', temp_list);
+//     console.log('index is :>> ', index);
+//     temp_list.splice(index,1);
+
+//     console.log('temp_list after delete is   :>> ', temp_list );
+//     setTaskList(temp_list);
+//   }
+
+//   return (
+//     <div className = 'main-container'>
+//       <div className = 'center-container'>
+//         <TodoInput addItem = {add_item_to_list}  />
+//         {
+//           taskList.map((ele,index) =>{
+//             return <TodoList key={index} current_index={index} list_item={ele} delete_item={delete_item_from_list} />
+//           })
+//         }
+      
+//       </div>
+//     </div>
+//   )
+
+
+// }
+
+// export default App
+
+
